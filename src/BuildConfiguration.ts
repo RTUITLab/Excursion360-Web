8e7f6797b8372e7bcdd67defb56321e889385aa3
Maksim <maksalmak@gmail.com>
2019-11-15 09:16:47 +0300

    Use all configuration, update README

--- src/BuildConfiguration.ts
+++ src/BuildConfiguration.ts
@@ -1,6 +1,6 @@
 export class BuildConfiguration {
     public static get ConfigFilePath(): string {
-        return process.env.CONFIG_FILE_PATTH;
+        return process.env.CONFIG_FILE_PATH;
     }
     public static get NeedDebugLayer(): string {
         return process.env.NEED_DEBUG_LAYER;
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Viewer } from "./Viewer";
 import { Configuration } from "./Configuration";
 import axios from "axios";
 import { Excursion } from "./Models/ExcursionModels/Excursion";
+import { BuildConfiguration } from "./BuildConfiguration";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const configuration = await axios.get<Configuration>("config.json");
+    const configuration = await axios.get<Configuration>(BuildConfiguration.ConfigFilePath || "config.json");
     if (configuration.status !== 200) {
         console.warn("Can't get configuration");
         return;
