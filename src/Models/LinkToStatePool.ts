d64659f5029cf775622b5cba262042e26ccc8a26
Maksim Makushchenko <maksalmak@gmail.com>
2020-02-10 19:15:15 +0300

    Add group connection creating

--- src/Models/LinkToStatePool.ts
+++ src/Models/LinkToStatePool.ts
@@ -1,5 +1,6 @@
 import { LinkToState } from "./LinkToState";
 import { Scene, Vector3, Material } from "babylonjs";
+import { GroupLink } from "./GroupLink";
 
 export class LinkToStatePool {
 
@@ -21,6 +22,15 @@ export class LinkToStatePool {
         material: Material,
         triggered: () => Promise<void>): LinkToState {
         const link = new LinkToState(name, position, material, triggered, this.scene);
+        this.links.push(link);
+        return link;
+    }
+    public getGroupLink(
+        name: string,
+        position: Vector3,
+        material: Material,
+        triggered: () => Promise<void>): LinkToState {
+        const link = new GroupLink(name, position, material, triggered, this.scene);
         this.links.push(link);
         return link;
     }
