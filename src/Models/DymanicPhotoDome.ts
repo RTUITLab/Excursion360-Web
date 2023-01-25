03e6d87468db9292eb8bc9c22cd48d67647f2835
Maksim Makushchenko <maksalmak@gmail.com>
2023-01-25 17:54:51 +0300

    Support webgl max texture size

--- src/Models/DymanicPhotoDome.ts
+++ src/Models/DymanicPhotoDome.ts
@@ -12,6 +12,8 @@ export default class DynamicPhotoDome {
 
   private canvasWidth: number = 0;
   private canvasHeight: number = 0;
+  private textureMultipler: number = 1;
+  private maxTextureSize: number = 0;
 
   private findRectangleStepW: number = 0;
   private findRectangleStepH: number = 0;
@@ -23,6 +25,10 @@ export default class DynamicPhotoDome {
   private loadedImageParts: Set<CroppedImagePart> = new Set();
 
   constructor(size: number, scene: Scene) {
+    const canvas = scene.getEngine().getRenderingCanvas();
+    const gl = canvas.getContext("webgl") || canvas.getContext("webgl2");
+    this.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+
     this.texture = new DynamicTexture("photodome dynamic texture", { width: 0, height: 0 }, scene, false);
     this.drawContext = this.texture.getContext();
     this.photoDome = new PhotoDome("photodome", null, { resolution: 32, size }, scene);
@@ -66,8 +72,9 @@ export default class DynamicPhotoDome {
           image.crossOrigin = "anonymous";
           const drawContext = this.drawContext;
           const texture = this.texture;
+          const multipler = this.textureMultipler;
           image.onload = function (this: HTMLImageElement) {
-            drawContext.drawImage(this, 0, 0, part.width, part.height, part.x, part.y, part.width, part.height);
+            drawContext.drawImage(this, 0, 0, part.width, part.height, part.x * multipler, part.y * multipler, part.width * multipler, part.height * multipler);
             texture.update(false);
           }
           image.src = `${this.baseRoute}/${part.route}`;
@@ -81,7 +88,7 @@ export default class DynamicPhotoDome {
       return null;
     }
     const textureSize = this.texture.getSize();
-    const { x, y } = pickInfo.getTextureCoordinates().multiplyByFloats(textureSize.width, textureSize.height);
+    const { x, y } = pickInfo.getTextureCoordinates().multiplyByFloats(textureSize.width / this.textureMultipler, textureSize.height / this.textureMultipler);
     const target = this.imagePartsToLoad.find(r => x >= r.x && y >= r.y && x <= r.x + r.width && y <= r.y + r.height);
     if (!target) {
       return null;
@@ -96,7 +103,13 @@ export default class DynamicPhotoDome {
   public setByImage(image: HTMLImageElement, size?: { width: number, height: number }) {
     this.imagePartsToLoad = null;
     this.loadedImageParts.clear();
-    const { width, height } = size || { width: image.width, height: image.height };
+    let { width, height } = size || { width: image.width, height: image.height };
+    if (width > this.maxTextureSize || height > this.maxTextureSize) {
+      const max = Math.max(width, height);
+      this.textureMultipler = this.maxTextureSize / max;
+      width *= this.textureMultipler;
+      height *= this.textureMultipler;
+    }
     this.texture.scaleTo(width, height);
     this.drawContext.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
     this.texture.update(false);
